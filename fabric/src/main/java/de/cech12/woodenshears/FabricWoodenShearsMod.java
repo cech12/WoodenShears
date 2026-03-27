73b760d1d9f8b8ea296fa8045ab9bf26b34d1cc1
cech12 <cech12@gmail.com>
2026-03-27 17:23:05 +0100

    updated to Minecraft 26.1 (Fabric 0.144.3+26.1, NeoForge 26.1.0.6-beta, Forge 62.0.3)

--- fabric/src/main/java/de/cech12/woodenshears/FabricWoodenShearsMod.java
+++ fabric/src/main/java/de/cech12/woodenshears/FabricWoodenShearsMod.java
@@ -2,7 +2,7 @@ package de.cech12.woodenshears;
 
 import de.cech12.woodenshears.item.WoodenShearsItem;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.dispenser.ShearsDispenseItemBehavior;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -39,7 +39,7 @@ public class FabricWoodenShearsMod implements ModInitializer {
     public void onInitialize() {
         CommonLoader.init();
         //Register item in the creative tab.
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(content -> {
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register(content -> {
             content.accept(WOODEN_SHEARS);
         });
         //register dispense behavior

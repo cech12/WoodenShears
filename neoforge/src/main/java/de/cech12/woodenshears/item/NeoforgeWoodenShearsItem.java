9e1ed59d23bb2adae86fe22b296a030cb222329d
cech12 <cech12@gmail.com>
2026-05-14 07:41:50 +0200

    added the possibility to use wooden shears as fuel in furnaces like wooden shovels (all loaders) #39

--- neoforge/src/main/java/de/cech12/woodenshears/item/NeoforgeWoodenShearsItem.java
+++ neoforge/src/main/java/de/cech12/woodenshears/item/NeoforgeWoodenShearsItem.java
@@ -1,7 +1,10 @@
 package de.cech12.woodenshears.item;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.block.entity.FuelValues;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class NeoforgeWoodenShearsItem extends WoodenShearsItem {
 
@@ -17,6 +20,11 @@ public class NeoforgeWoodenShearsItem extends WoodenShearsItem {
     @Override
     public int getMaxDamage(@NotNull ItemStack stack) {
         return stack.getMaxDamage(); //references to mixed in method
+    }
+
+    @Override
+    public int getBurnTime(@NotNull ItemStack itemStack, @Nullable RecipeType<?> recipeType, @NotNull FuelValues fuelValues) {
+        return this.getShearsBurnTime();
     }
 
 }
